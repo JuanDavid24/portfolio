860a41b805b068af0a1a7c645abc95326c1fe463
JuanDavid24 <31551453+JuanDavid24@users.noreply.github.com>
2025-05-07 23:51:55 -0300

    [STYLE] improved responsive of whole page, simplified project cards and used grid, saturation effect on skillsadded some effects to skills

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,9 +3,9 @@ export function Projects() {
   return (
     <section id="projects">
       <h2 className="text-2xl mb-6">Proyectos</h2>
-      <ul className="flex flex-col gap-4">
+      <ul className="grid md:grid-cols-2 grid-cols-1 gap-4 justify-items-center align-top">
         {projectsData.map(project => (
-          <li>
+          <li className="max-w-lg ">
             <ProjectCard {...project} />
           </li>
         ))}
@@ -16,20 +16,40 @@ export function Projects() {
 
 function ProjectCard({ name, description, images, tags }) {
   return (
-    <article className="min-h-50 project-card flex flex-col md:flex-row md:p-0 border-1 rounded-2xl">
-      <img className="md:w-60 rounded-t-2xl md:rounded-l-2xl md:rounded-r-none object-cover" src={images[0]} alt={`image 1 of project ${name}`} />
-      <div className="project-card__body flex flex-col gap-2 justify-around md:px-4 md:pb-4 px-2 pb-2">
-        <h3 className="text-xl">{name}</h3>
-        <div className="project-card__description">{description}</div>
-        <div className="project-card__tags">
-          {tags.map(tag => <span>{tag}</span>)}
-        </div>
-        <div className="project-card__footer">
-          <a href="www.github.com">Github</a>
-          <a href="www.project.com">Demo</a>
-        </div>
-      </div>
-
+    <article className="border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700 overflow-hidden">
+      <a href="#">
+        <img className="h-100 object-cover saturate-50" src={images[0]} alt={`image 1 of project ${name}`} />
+      </a>
+      <h3 className="text-lg text-center py-2">{name}</h3>
     </article>
+  )
+}
+
+//TODO: modal
+// function ProjectModal({ name, description, images, tags }) {
+//   return (
+//     <article className="min-h-50 project-card flex flex-col md:flex-row md:p-0 border-1 rounded-2xl">
+//       <img className="md:w-60 rounded-t-2xl md:rounded-l-2xl md:rounded-r-none object-cover" src={images[0]} alt={`image 1 of project ${name}`} />
+//       <div className="project-card__body flex flex-col gap-2 justify-around md:px-4 md:pb-4 px-2 pb-2">
+//         <h3 className="text-xl">{name}</h3>
+//         <div className="project-card__description">{description}</div>
+//         <ul className="project-card__tags flex flex-row flex-wrap gap-x-1 gap-y-3">
+//           {tags.map(tag =>
+//             <li>
+//               <Tag text={tag} />
+//             </li>)}
+//         </ul>
+//         <div className="project-card__footer">
+//           <a href="www.github.com">Github</a>
+//           <a href="www.project.com">Demo</a>
+//         </div>
+//       </div>
+//     </article>
+//   )
+// }
+
+function Tag({ text }) {
+  return (
+    <span className="border-2 rounded-3xl py-1 px-2 text-emerald-700">{text}</span>
   )
 }
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -4,7 +4,7 @@ export function Skills() {
   return (
     <section id="skills" className="skills">
       <h2 className="text-2xl mb-6">Habilidades</h2>
-      <ul className="flex justify-center mx-auto">
+      <ul className="flex flex-wrap justify-center gap-2 mx-auto w-11/12">
         {skillsData.map(({ name, logo }) => (
           <li className="w-24">
             <SkillCard skillName={name} skillLogo={logo} />
@@ -17,8 +17,8 @@ export function Skills() {
 
 export function SkillCard({ skillName, skillLogo }) {
   return (
-    <div className="skill-card">
-      <img src={skillLogo} alt={skillName} />
+    <div className="flex flex-col items-center p-3 bg-teal-400/10 bg-opacity-70 rounded-2xl hover:scale-110 transition duration-300">
+      <img className="w-12 h-12 opacity-100 hover:saturate-100 saturate-25 transition delay-150 duration-300" src={skillLogo} alt={skillName} />
       <div className="skill-name">{skillName}</div>
     </div>
   )
