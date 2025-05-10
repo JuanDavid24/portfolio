983a8b7388aee9196172681cb177c9cf77e71c0d
katia <kautuuu@gmail.com>
2025-05-10 20:58:29 -0300

    [REFAC] changed skill list manage in map + prettier formating [STYLE] wrap row in links list [FIX] key warning in skills

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,42 @@
 export function Header() {
-    return(
+  return (
     <header>
-    <nav className="w-screen text-gray-50 bg-gray-950 fixed top-0 z-1">
-      <ul className="flex justify-center gap-3 py-5 opacity-100">
-        <li className="my-1"><a className="p-5 border-2 border-gray-950 hover:border-teal-500 hover:text-teal-600 hover:size-1.5 duration-200 ease-in rounded-full" href="#">Inicio</a></li>
-        <li className="my-1"><a className="p-5 border-2 border-gray-950 hover:border-teal-500 hover:text-teal-600 hover:size-1.5 duration-200 ease-in rounded-full" href="#skills">Habilidades</a></li>
-        <li className="my-1"><a className="p-5 border-2 border-gray-950 hover:border-teal-500 hover:text-teal-600 hover:size-1.5 duration-200 ease-in rounded-full" href="#projects">Proyectos</a></li>
-        <li className="my-1"><a className="p-5 border-2 border-gray-950 hover:border-teal-500 hover:text-teal-600 hover:size-1.5 duration-200 ease-in rounded-full" href="#contact">Contacto</a></li>
-      </ul>
-    </nav>
-  </header>
-    )
+      <nav className="w-screen text-gray-50 bg-gray-950 fixed top-0 z-1">
+        <ul className="flex justify-center gap-3 py-5 opacity-100">
+          <li className="my-1">
+            <a
+              className="p-5 border-2 border-gray-950 hover:border-teal-500 hover:text-teal-600 hover:size-1.5 duration-200 ease-in rounded-full"
+              href="#"
+            >
+              Inicio
+            </a>
+          </li>
+          <li className="my-1">
+            <a
+              className="p-5 border-2 border-gray-950 hover:border-teal-500 hover:text-teal-600 hover:size-1.5 duration-200 ease-in rounded-full"
+              href="#skills"
+            >
+              Habilidades
+            </a>
+          </li>
+          <li className="my-1">
+            <a
+              className="p-5 border-2 border-gray-950 hover:border-teal-500 hover:text-teal-600 hover:size-1.5 duration-200 ease-in rounded-full"
+              href="#projects"
+            >
+              Proyectos
+            </a>
+          </li>
+          <li className="my-1">
+            <a
+              className="p-5 border-2 border-gray-950 hover:border-teal-500 hover:text-teal-600 hover:size-1.5 duration-200 ease-in rounded-full"
+              href="#contact"
+            >
+              Contacto
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,29 +1,38 @@
-import { skillsData } from "../data/skills"
+import { skillsData } from "../data/skills";
 
 export function Skills() {
+  const skillList = skillsData.map(({ name, logo }, index) => (
+    <li key={name} className="w-24">
+      <SkillCard
+        skillName={name}
+        skillLogo={logo}
+        animationDelay={`${index * 0.05}s`}
+      />
+    </li>
+  ));
   return (
     <section id="skills" className="skills">
       <h2 className="text-2xl mb-6 text-center">Habilidades</h2>
       <ul className="flex flex-wrap justify-center gap-2 mx-auto w-11/12">
-        {skillsData.map(({ name, logo }, index) => (
-          <li className="w-24">
-            <SkillCard skillName={name} skillLogo={logo} animationDelay={`${index * 0.05}s`} />
-          </li>
-        ))}
+        {skillList}
       </ul>
     </section>
-  )
+  );
 }
 
 export function SkillCard({ skillName, skillLogo, animationDelay }) {
   return (
-    <div 
+    <div
       className="flex flex-col items-center p-3 bg-teal-400/10 bg-opacity-70 rounded-2xl
-        hover:scale-110 duration-300 animate-slide-in-bottom animate-duration-slow cursor-pointer group" 
+        hover:scale-110 duration-300 animate-slide-in-bottom animate-duration-slow cursor-pointer group"
       style={{ animationDelay }}
     >
-      <img className="w-12 h-12 opacity-100 group-hover:brightness-100 group-hover:saturate-100 brightness-75 saturate-75 transition delay-150 duration-300" src={skillLogo} alt={skillName} />
+      <img
+        className="w-12 h-12 opacity-100 group-hover:brightness-100 group-hover:saturate-100 brightness-75 saturate-75 transition delay-150 duration-300"
+        src={skillLogo}
+        alt={skillName}
+      />
       <div className="skill-name">{skillName}</div>
     </div>
-  )
+  );
 }
