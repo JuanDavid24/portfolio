39e7d6308f463a146cf838b83c851b34c296a5eb
katia <kautuuu@gmail.com>
2025-05-12 23:47:23 -0300

    [FEAT] modal with full project card - LinkButton icon size parameter [FIX] project cards didn't center text div in some cases

---
+++ src/components/Modal.jsx
@@ -0,0 +1,22 @@
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+export function Modal({ children, isOpen, onClose }) {
+  if (isOpen) {
+    return (
+      <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50">
+        <div className="bg-white dark:bg-gray-900 w-full sm:max-w-11/12 p-6 rounded-lg relative shadow-lg">
+          {/* Botón cerrar */}
+          <button
+            className="absolute sm:-top-2 sm:-right-6 md:-top-8 md:-right-10 sm:text-4xl text-gray-500 hover:text-red-500 duration-500 cursor-pointer"
+            onClick={onClose}
+          >
+            <FontAwesomeIcon icon={faXmark} />
+          </button>
+          {/* Contenido del modal */}
+          <div className="content">{children}</div>
+        </div>
+      </div>
+    );
+  }
+}
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,84 +1,106 @@
-import { faGithub } from "@fortawesome/free-brands-svg-icons"
-import { projectsData } from "../data/projects"
-import { LinkButton } from "./LinksList"
+import { useState } from "react";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
+import { projectsData } from "../data/projects";
+import { LinkButton } from "./LinksList";
+import { Modal } from "./Modal";
 
 export function Projects() {
+  const [selectedProject, setSelectedProject] = useState(null);
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const handleCloseModal = () => {
+    setSelectedProject(null)
+  }
+
+  const handleOpenModal = (project) => {
+    setSelectedProject(project)
+    setModalOpen(true)
+  }
   return (
     <section className="flex flex-col items-center" id="projects">
       <h2 className="text-2xl mb-6 text-center">Proyectos</h2>
       {/* <ProjectExpandedCard {...projectsData[0]} /> */}
       <ul className="grid md:grid-cols-2 grid-cols-1 gap-4 justify-items-center align-top">
-        {projectsData.map(project => (
+        {projectsData.map((project) => (
           <li className="max-w-lg" key={project.name}>
-            <ProjectCard {...project} />
+            <MiniProjectCard
+              project={project}
+              handleClick={handleOpenModal}
+            />
           </li>
         ))}
       </ul>
+      {selectedProject && (
+        <Modal
+          isOpen={modalOpen}
+          onClose={handleCloseModal}>
+          <ProjectCard project={selectedProject} />
+        </Modal>
+      )}
     </section>
-  )
+  );
 }
 
-export function ProjectCard({ name, description, images, tags }) {
+export function MiniProjectCard({ project, handleClick }) {
+  const { name, images } = project;
   return (
-    <article className="relative border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700 overflow-hidden 
-      hover:animate-squeeze hover:scale-125 hover:z-10 hover:shadow-teal-800 hover:shadow-lg duration-150 delay-150 animate-duration-faster animate-ease-in-out">
-      <a href="#">
-        <img className="h-100 object-cover saturate-40 hover:saturate-100 duration-150" src={images[0]} alt={`image 1 of project ${name}`} />
+    <article
+      className="relative border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700 overflow-hidden cursor-pointer
+      hover:animate-squeeze hover:scale-125 hover:z-10 hover:shadow-teal-800 hover:shadow-lg duration-150 delay-150 animate-duration-faster animate-ease-in-out"
+    >
+      <a onClick={() => handleClick(project)}>
+        <img
+          className="h-100 object-cover saturate-40 hover:saturate-100 duration-150"
+          src={images[0]}
+          alt={`image 1 of project ${name}`}
+        />
       </a>
       <h3 className="text-lg text-center py-2">{name}</h3>
     </article>
-  )
+  );
 }
 
-function ProjectExpandedCard({ name, description, images, tags, repository, demo }) {
+function ProjectCard({ project }) {
+  const { name, description, images, tags, repository, demo } = project;
   return (
-    <article className="flex lg:flex-row flex-col z-50 justify-between rounded-2xl border-2 w-full">
-      <img className="width-full lg:max-w-2/3 aspect-video object-cover rounded-2xl" src={images[0]} alt={`imagen del proyecto ${name}`} />
-      <div className="flex flex-col text-center gap-2 justify-evenly items-center p-2">
+    <article className="flex lg:flex-row flex-col z-50 rounded-2xl border-2 w-full">
+      <img
+        className="width-full lg:max-w-2/3 aspect-video object-cover rounded-2xl"
+        src={images[0]}
+        alt={`imagen del proyecto ${name}`}
+      />
+      <div className="flex flex-col text-center gap-2 mx-auto justify-evenly items-center p-2">
         <h3 className="text-2xl">{name}</h3>
         <p>{description}</p>
         <ul className="flex flex-wrap gap-2 justify-center">
-          {tags.map(tag => (
-            <li> 
-              <Tag text={tag} color="teal-800" /> 
+          {tags.map((tag) => (
+            <li>
+              <Tag text={tag} color="teal-800" />
             </li>
-            ))}
+          ))}
         </ul>
         <ul className="flex gap-2 mt-4 lg:mt-0">
-          {repository && <li><LinkButton name="Repositorio" url={repository} icon={faGithub}/></li>}
-          {demo && <li><LinkButton name="Demo" url={demo} /></li>}
+          {repository && (
+            <li>
+              <LinkButton text="Repositorio" url={repository} icon={{iconName:faGithub, iconSize:"xl"}} />
+            </li>
+          )}
+          {demo && (
+            <li>
+              <LinkButton text="Demo" url={demo} icon={{iconName:faArrowUpRightFromSquare, iconSize:"xl"}}/>
+            </li>
+          )}
         </ul>
       </div>
     </article>
-  )
+  );
 }
 
 function Tag({ text, color }) {
-  return(
-    <span className="text-teal-400 border-teal-400 border-2 rounded-2xl p-1">{text}</span>
-  )
+  return (
+    <span className="text-teal-400 border-teal-400 border-2 rounded-2xl p-1">
+      {text}
+    </span>
+  );
 }
-
-//TODO: modal
-// function ProjectModal({ name, description, images, tags }) {
-//   return (
-//     <article className="min-h-50 project-card flex flex-col md:flex-row md:p-0 border-1 rounded-2xl">
-//       <img className="md:w-60 rounded-t-2xl md:rounded-l-2xl md:rounded-r-none object-cover" src={images[0]} alt={`image 1 of project ${name}`} />
-//       <div className="project-card__body flex flex-col gap-2 justify-around md:px-4 md:pb-4 px-2 pb-2">
-//         <h3 className="text-xl">{name}</h3>
-//         <div className="project-card__description">{description}</div>
-//         <ul className="project-card__tags flex flex-row flex-wrap gap-x-1 gap-y-3">
-//           {tags.map(tag =>
-//             <li>
-//               <Tag text={tag} />
-//             </li>)}
-//         </ul>
-//         <div className="project-card__footer">
-//           <a href="www.github.com">Github</a>
-//           <a href="www.project.com">Demo</a>
-//         </div>
-//       </div>
-//     </article>
-//   )
-// }
-
